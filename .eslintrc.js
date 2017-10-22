848a850bbc6f817760b59c67ad1529345a8bb7d6
Christophe Hurpeau <christophe@hurpeau.com>
2017-10-22 15:36:17 +0200

    chore: update dependencies

--- .eslintrc.js
+++
@@ -1,1 +0,0 @@
-exports.extends = 'pob/base';
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* eslint-disable global-require */
 
+'use strict';
+
 function preset(context, opts) {
   opts = opts || {}; // support node 4
   const modules = opts.modules !== undefined ? opts.modules : 'commonjs';
@@ -12,15 +14,29 @@ function preset(context, opts) {
   const esnext = opts.esnext !== undefined ? opts.esnext : true;
 
   if (modules !== false && modules !== 'commonjs') {
-    throw new Error("Preset es2015 'modules' option must be 'false' to indicate no modules\n" +
-        "or 'commonjs' (default)");
+    throw new Error(
+      "Preset es2015 'modules' option must be 'false' to indicate no modules\n" +
+        "or 'commonjs' (default)"
+    );
   }
-  if (typeof safari10 !== 'boolean') throw new Error("Preset modern-browsers 'safari10' option must be a boolean.");
-  if (typeof edge !== 'boolean') throw new Error("Preset modern-browsers 'edge' option must be a boolean.");
-  if (typeof loose !== 'boolean') throw new Error("Preset modern-browsers 'loose' option must be a boolean.");
-  if (typeof es2016 !== 'boolean') throw new Error("Preset modern-browsers 'es2016' option must be a boolean.");
-  if (typeof es2017 !== 'boolean') throw new Error("Preset modern-browsers 'es2017' option must be a boolean.");
-  if (typeof esnext !== 'boolean') throw new Error("Preset modern-browsers 'esnext' option must be a boolean.");
+  if (typeof safari10 !== 'boolean') {
+    throw new Error("Preset modern-browsers 'safari10' option must be a boolean.");
+  }
+  if (typeof edge !== 'boolean') {
+    throw new Error("Preset modern-browsers 'edge' option must be a boolean.");
+  }
+  if (typeof loose !== 'boolean') {
+    throw new Error("Preset modern-browsers 'loose' option must be a boolean.");
+  }
+  if (typeof es2016 !== 'boolean') {
+    throw new Error("Preset modern-browsers 'es2016' option must be a boolean.");
+  }
+  if (typeof es2017 !== 'boolean') {
+    throw new Error("Preset modern-browsers 'es2017' option must be a boolean.");
+  }
+  if (typeof esnext !== 'boolean') {
+    throw new Error("Preset modern-browsers 'esnext' option must be a boolean.");
+  }
 
   const optsLoose = { loose };
 
@@ -32,17 +48,19 @@ function preset(context, opts) {
       edge && require('babel-plugin-transform-es2015-arrow-functions'), // needed for function-name
       edge && require('babel-plugin-transform-es2015-function-name'),
 
-      modules === 'commonjs' && [require('babel-plugin-transform-es2015-modules-commonjs'), optsLoose],
+      modules === 'commonjs' && [
+        require('babel-plugin-transform-es2015-modules-commonjs'),
+        optsLoose,
+      ],
 
       /* es2017 */
       es2017 && require('babel-plugin-syntax-trailing-function-commas'),
 
       /* esnext */
-      esnext && (
-        (safari10 || edge)
+      esnext &&
+        (safari10 || edge
           ? require('babel-plugin-transform-object-rest-spread')
-          : require('babel-plugin-syntax-object-rest-spread')
-      ),
+          : require('babel-plugin-syntax-object-rest-spread')),
     ].filter(Boolean),
   };
 }
